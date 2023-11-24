2499d54d8f4a5fe2759a59f6c6fdb9515909416a
JesseBot <jessebot@linux.com>
2023-11-24 13:15:12 +0100

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -2,5 +2,36 @@ module.exports = {
     extends: ['config:recommended'],
     allowPostUpgradeCommandTemplating: true,
     allowedPostUpgradeCommands: ['^.*'],
-    repositories: ['small-hack/matrix-chart']
+    repositories: ['small-hack/matrix-chart'],
+    platform: 'github',
+    includeForks: true,
+    regexManagers: [
+        {
+            fileMatch: ['(^|/)Chart\\.yaml$'],
+            matchStrings: [
+                '#\\s?renovate: image=(?<depName>.*?)\\s?appVersion:\\s?\\"?(?<currentValue>[\\w+\\.\\-]*)',
+            ],
+            datasourceTemplate: 'docker',
+        }
+    ],
+    packageRules: [
+        {
+            matchManagers: ['helm-requirements', 'helm-values', 'regex'],
+            postUpgradeTasks: {
+                commands: [
+                  `version=$(grep '^version:' {{{parentDir}}}/Chart.yaml | awk '{print $2}')
+                  major=$(echo $version | cut -d. -f1)
+                  minor=$(echo $version | cut -d. -f2)
+                  patch=$(echo $version | cut -d. -f3)
+                  minor=$(expr $minor + 1)
+                  echo "Replacing $version with $major.$minor.$patch"
+                  sed -i "s/^version:.*/version: $\{major\}.$\{minor\}.$\{patch\}/g" {{{parentDir}}}/Chart.yaml
+                  cat {{{parentDir}}}/Chart.yaml
+                  `,
+                ],
+                fileFilters: ['**/Chart.yaml'],
+                executionMode: 'branch'
+            }
+        }
+    ]
 };
